fb21f5a1c8a403c13dfbadeb0fa5b492dfe039a4
www-data <www-data@TBOCS.(none)>
2012-11-11 12:46:55 +0900

    bhtak add ocs test case handlers

--- sites/all/modules/ocs_admin/js/testcase.js
+++ sites/all/modules/ocs_admin/js/testcase.js
@@ -8,22 +8,60 @@
 
   $.fn.runTestOcsHandler = function(event) {
   	console.log('run test case');
+    /*
+    $('#run-test-ocs-result').append( 
+    '<table>' +
+    '<thead><tr><th>Response</th><th>Time</th></tr></thead>' +
+    '<tbody></tbody>' +
+    '</table>'
+    );
+    */
 	var ts = $('.testcase_status').each( function(index){
         $(this).text( 'Requesting');
 
-        // get URL
-        var testOcsUrl = '/ajax/test/ocs/' + $(this).attr('node');
-        $('#run-test-ocs-result > ul').append( '<li>' + testOcsUrl + '</li>');
-
+        // first, get the number of test cases
+        var nid = $(this).attr('node');
+        var testOcsUrl = '/ajax/test/ocs/count/' + nid;
         $.ajax({
             url: testOcsUrl,
             type: "get",
             success: function(data) {
                 var output = data;
                 $('#run-test-ocs-result > ul').append( '<li>' + output + '</li>');
+                $.fn.runOcsTestCase(nid, data);
+            },
+            error: function() {
+                $('#run-test-ocs-result > ul').append( '<li> Fail: ' + testOcsUrl + '</li>');
             }
         });
+
+        
     });
   };
+
+    $.fn.runOcsTestCase = function(nid,testData) {
+        var testcase = $.parseJSON(testData);
+        console.log( 'test: ', testcase);
+        for( var i=0; i< testcase.length; i++) {
+            var testOcsUrl = '/ajax/test/ocs/' + nid + '/' + testcase[i].nid;
+            $('#run-test-ocs-result > ul').append( '<li>' + testOcsUrl + '</li>');
+
+            $('.testcase_status[node=' + nid + ']').append( '<li node="' + nid + ':' + testcase[i].nid + '"> REQ ' + testOcsUrl + '</li>');
+            $.ajax({
+                url: testOcsUrl,
+                type: "get",
+                success: function(data) {
+                    var result = $.parseJSON(data);
+                    $('#run-test-ocs-result > ul').append( data);
+                    console.log( result);
+
+                    $('li[node="' + result.nid + ':' + result.tc_nid + '"]').append( 
+                        '<ul><li> ' + result.nid  + ' : ' + result.effective_url + '</li>'  +
+                        '<li> RSP  : ' + result.response + ', TIME : ' + result.total_time + '</li></ul>'  );
+
+                }
+            });
+        }
+    }
 	
  })(jQuery);
