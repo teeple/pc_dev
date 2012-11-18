82b8acb2442c17cb9969a796bde491f258d5f39f
BohunTak <bhtak@BohunTak-MackBookPro.local>
2012-11-18 10:26:02 +0900

    add javascripts to handle ajax form in /lifecycles/scheme/%nid/action/add

--- sites/all/modules/ocs_admin/js/testcase.js
+++ sites/all/modules/ocs_admin/js/testcase.js
@@ -1,70 +1,83 @@
-(function($){
-  Drupal.behaviors.ocs_admin = {
-	attach: function(context, settings) {
-		console.log('testcase.js loaded');
-		$('#run-test-ocs-button').bind('click',$.fn.runTestOcsHandler);
-	}
-  };
+(function($) {
+	Drupal.behaviors.ocs_admin = {
+		attach : function(context, settings) {
+			console.log('testcase.js loaded');
+			$('#run-test-ocs-button').bind('click', $.fn.runTestOcsHandler); // test button
+			$('#add-action-button').bind('click', $.fn.addActionHandler);
+		}
+	};
 
-  $.fn.runTestOcsHandler = function(event) {
-  	console.log('run test case');
-    /*
-    $('#run-test-ocs-result').append( 
-    '<table>' +
-    '<thead><tr><th>Response</th><th>Time</th></tr></thead>' +
-    '<tbody></tbody>' +
-    '</table>'
-    );
-    */
-	var ts = $('.testcase_status').each( function(index){
-        $(this).text( 'Requesting');
+	$.fn.runTestOcsHandler = function(event) {
+		console.log('run test case');
+		/*
+		$('#run-test-ocs-result').append( 
+		'<table>' +
+		'<thead><tr><th>Response</th><th>Time</th></tr></thead>' +
+		'<tbody></tbody>' +
+		'</table>'
+		);
+		 */
+		var ts = $('.testcase_status').each(
+				function(index) {
+					$(this).text('Requesting');
 
-        // first, get the number of test cases
-        var nid = $(this).attr('node');
-        var testOcsUrl = '/ajax/test/ocs/count/' + nid;
-        $.ajax({
-            url: testOcsUrl,
-            type: "get",
-            success: function(data) {
-                var output = data;
-//                $('#run-test-ocs-result > ul').append( '<li>' + output + '</li>');
-                $.fn.runOcsTestCase(nid, data);
-            },
-            error: function() {
-                $('#run-test-ocs-result > ul').append( '<li> Fail: ' + testOcsUrl + '</li>');
-            }
-        });
+					// first, get the number of test cases
+					var nid = $(this).attr('node');
+					var testOcsUrl = '/ajax/test/ocs/count/' + nid;
+					$.ajax({
+						url : testOcsUrl,
+						type : "get",
+						success : function(data) {
+							var output = data;
+							//                $('#run-test-ocs-result > ul').append( '<li>' + output + '</li>');
+							$.fn.runOcsTestCase(nid, data);
+						},
+						error : function() {
+							$('#run-test-ocs-result > ul').append(
+									'<li> Fail: ' + testOcsUrl + '</li>');
+						}
+					});
 
-        
-    });
-  };
+				});
+	};
 
-    $.fn.runOcsTestCase = function(nid,testData) {
-        var testcase = $.parseJSON(testData);
-        console.log( 'test: ', testcase);
-        for( var i=0; i< testcase.length; i++) {
-            var testOcsUrl = '/ajax/test/ocs/' + nid + '/' + testcase[i].nid;
-//            $('#run-test-ocs-result > ul').append( '<li>' + testOcsUrl + '</li>');
+	$.fn.runOcsTestCase = function(nid, testData) {
+		var testcase = $.parseJSON(testData);
+		console.log('test: ', testcase);
+		for ( var i = 0; i < testcase.length; i++) {
+			var testOcsUrl = '/ajax/test/ocs/' + nid + '/' + testcase[i].nid;
+			//            $('#run-test-ocs-result > ul').append( '<li>' + testOcsUrl + '</li>');
 
-            $('.testcase_status[node=' + nid + ']').append( '<li node="' + nid + ':' + testcase[i].nid + '"> REQ ' + testOcsUrl + '</li>');
-            $.ajax({
-                url: testOcsUrl,
-                type: "get",
-                success: function(data) {
-                    var r = $.parseJSON(data);
-                    /*
-                    $('#run-test-ocs-result > ul').append( data);
-                    */
-                    console.log( r);
+			$('.testcase_status[node=' + nid + ']').append(
+					'<li node="' + nid + ':' + testcase[i].nid + '"> REQ '
+							+ testOcsUrl + '</li>');
+			$.ajax({
+				url : testOcsUrl,
+				type : "get",
+				success : function(data) {
+					var r = $.parseJSON(data);
+					/*
+					$('#run-test-ocs-result > ul').append( data);
+					 */
+					console.log(r);
 
-                    $('li[node="' + r.nid + ':' + r.tc_nid + '"]').append( 
-                        '<ul><li> ' + r.nid  + ' : ' + r.response.request + '</li>'  +
-                        '<li> RSP  : ' + r.response.code + r.response.status_message + '</li>' +
-                        '<li>' + r.response.data + '</li></ul>'  );
+					$('li[node="' + r.nid + ':' + r.tc_nid + '"]').append(
+							'<ul><li> ' + r.nid + ' : ' + r.response.request
+									+ '</li>' + '<li> RSP  : '
+									+ r.response.code
+									+ r.response.status_message + '</li>'
+									+ '<li>' + r.response.data + '</li></ul>');
 
-                }
-            });
-        }
-    }
+				}
+			});
+		}
+	};
 	
- })(jQuery);
+	$.fn.addActionHandler = function() {
+		var num = $('input[name="num_action"]').val();
+		console.log('add action', num);
+		$('div.action-' + num).removeClass('hide');
+		$('input[name="num_action"]').val( parseInt(num)+1);
+	};
+
+})(jQuery);
